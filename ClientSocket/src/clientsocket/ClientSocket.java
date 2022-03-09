2774f6e51339de29a4ed182d59c72b40fe316b51
Fumagalli-Luca <fumagalli.luca@studenti.salesianisesto.it>
2022-03-09 17:48:23 +0100

    Final Project

---
+++ ClientSocket/src/clientsocket/ClientSocket.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clientsocket;
+
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author fumagalli.luca
+ */
+public class ClientSocket {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            Socket clientSocket = new Socket("192.168.178.72", 5000);
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            Scanner tastiera = new Scanner(System.in);
+            boolean fine = false;
+            String messaggio = null;
+            while (!fine) {
+                System.out.println("Cosa vuoi scrivere al server?");
+                messaggio = tastiera.next();
+                if (messaggio.equals("fine")) {
+                    fine = true;
+                }
+                out.println(messaggio);
+                System.out.println("Il server risponde: " + in.readLine());
+            }
+            in.close();
+            out.close();
+            clientSocket.close();
+        } catch (IOException ex) {
+            Logger.getLogger(ClientSocket.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+}
---
+++ ServerSocket/src/serversocket/Server.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serverSocket;
+
+import java.net.*;
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author fumagalli.luca
+ */
+public class Server {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(5000);
+            System.out.println("Server partito");
+            Socket clientSocket;
+            ProcessoServer processo;
+            Thread T;
+            while (true) {
+                clientSocket = serverSocket.accept();
+                System.out.println("CONNESSIONE RICEVUTA: " + clientSocket.getInetAddress());
+                processo = new ProcessoServer(clientSocket);
+                T = new Thread(processo);
+                T.start();
+            }
+            //serverSocket.close();
+        } catch (IOException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+}
